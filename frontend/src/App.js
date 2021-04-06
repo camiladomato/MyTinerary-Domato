820e9757ed7b16bc3fc1889e6db0c44a86449efb
camila <camiladomato@gmail.com>
2021-04-05 23:29:50 -0300

    correciones al css 05-04

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react' //importar libreria
+import '-./App.css' //importar css
 
 //se puede poner export default antes de class o *// 
 class App extends React.Component{  //creo una clase de componente hereda de clase superior en libreria de react -component-
   render(){  //renderiza lo que retorna
-    var nombre= "Camila"  
+    var nombre= "Camila"  //forma de dar estilo:
     return (
-     <>
-       <h1>hola Soy {nombre} y esta es mi proxima web app de viajes</h1>
-      </>
+     <div className="contenedor"> 
+        <h1>hola Soy {nombre} y esta es Mi Proxima Web App de viajes</h1>
+        <h2>¡Bienvenidos!</h2>
+      </div>
     )
   }
 }
